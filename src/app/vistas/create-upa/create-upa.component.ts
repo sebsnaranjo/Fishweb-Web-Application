aa1e67d14d2dac2536aa2d0854b5dde3f3263f4c
Sebastian Naranjo <sebas2013n@gmail.com>
2021-11-20 14:47:52 -0500

    Ajustes para el consumo del servicio, Crear UPA

--- src/app/vistas/create-upa/create-upa.component.ts
+++ src/app/vistas/create-upa/create-upa.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UpasService } from 'src/app/servicios/upas.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-create-upa',
   templateUrl: './create-upa.component.html',
   styleUrls: ['./create-upa.component.css']
 })
-export class CreateUpaComponent implements OnInit {
+export class CreateUpaComponent {
 
-  constructor() { }
+  constructor(
+    private UpaService: UpasService,
+    private router:Router
+  ) { }
 
-  ngOnInit(): void {
+  crearUpaF = new FormGroup({
+    nameUpa: new FormControl('', Validators.required),
+    descript: new FormControl('', Validators.required),
+    numberAct: new FormControl('', Validators.required),
+    nameAdmin: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+  async createUPA(values) {
+    this.UpaService.crearUpa(values.nombre, values.descripcion, values.cantidadactividades).subscribe( (data) => {
+      console.log(data);
+      this.router.navigate(['inicio'])
+    })
+    //registro administrador
   }
 
 }
