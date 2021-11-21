8b42875415e5167eec6f9cdb1f4f35649041f65c
Sebastian Naranjo <sebas2013n@gmail.com>
2021-11-21 01:14:18 -0500

    Cambio de rol, boton para ello en la tabla de usuarios

--- src/app/vistas/gestionar-usuarios/gestionar-usuarios.component.ts
+++ src/app/vistas/gestionar-usuarios/gestionar-usuarios.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CreateUserDTO, User } from 'src/app/modelos/user.interface';
 import { ManagementusersService } from 'src/app/servicios/managementusers.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { UserChange } from 'src/app/modelos/userChange.interface';
+import { UrlSegment } from '@angular/router';
 
 @Component({
   selector: 'app-gestionar-usuarios',
@@ -11,17 +13,28 @@ import { MatTableDataSource } from '@angular/material/table';
 export class GestionarUsuariosComponent implements OnInit {
 
   ELEMENT_DATA: User[];
-  displayedColumns: string[] = ['nombre', 'apellido', 'fullName'];
+  displayedColumns: string[] = ['id', 'nombre', 'apellido', 'fullName', 'button'];
   dataSource = new MatTableDataSource<User>();
 
   constructor( private managmentUser:ManagementusersService) { }
 
   ngOnInit(): void {
     this.getUsers();
+    this.change();
   }
 
   public getUsers() {
     let resp=this.managmentUser.getAll();
     resp.subscribe(report => this.dataSource.data=report as User[])
+  }
+
+  public change(): void {
+    const updateUser: UserChange = {
+      idUser: 4,
+      rolUser: 3,
+    };
+    this.managmentUser.change(updateUser).subscribe((data)=>{
+      console.log(data)
+    })
   }
 }
