16eed6840e0151e51ee94bb0b03dd89c17199873
Sebastian Naranjo <sebas2013n@gmail.com>
2022-05-05 22:37:48 -0500

    Ajuste de direcciones

--- src/app/vistas/inicio/inicio-auxiliar/inicio-auxiliar.component.ts
+++ src/app/vistas/inicio/inicio-auxiliar/inicio-auxiliar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-auxiliar',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioAuxiliarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  redireccionar_controlar() {
+    this.router.navigate(['/controlar'])
+  }
+
+  /* se tiene que redireccionar al chat */
+  redireccionar_informar() {
+    this.router.navigate(['/inicio-auxiliar'])
   }
 
 }
--- src/app/vistas/inicio/inicio-superadminsitrador/inicio-superadminsitrador.component.ts
+++ src/app/vistas/inicio/inicio-superadminsitrador/inicio-superadminsitrador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-superadminsitrador',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioSuperadminsitradorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  redireccionar_controlar() {
+    this.router.navigate(['/controlar'])
+  }
+
+  redireccionar_usuarios() {
+    this.router.navigate(['/gestionar-usuarios'])
   }
 
 }
