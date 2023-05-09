e84f190a8c56e2cdc78a42b60b0b86ff6a7118b9
Sebastian Naranjo <sebas2013n@gmail.com>
2023-05-08 23:47:58 -0500

    Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  HOST: 'http://localhost:3000',
+  REGISTRO: 'http://localhost:3000',
+  //llave para acceder al token
+  TOKEN: 'access_token',
+  expiration: 'expiresIn',
+  rolId: 'rol_id',
+  userId: 'user_id',
+  upaId: 'upa_id',
+  email: 'email_user'
 };
