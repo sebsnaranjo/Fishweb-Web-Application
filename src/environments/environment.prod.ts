8d9ade136468152e85aa129942f256a772e6dfb9
Sebastian Naranjo <sebas2013n@gmail.com>
2023-05-09 00:51:33 -0500

    Ajuste heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   HOST: 'https://mysterious-brushlands-11324.herokuapp.com',
-  REGISTRO: 'http://localhost:3000',
+  REGISTRO: 'https://mysterious-brushlands-11324.herokuapp.com',
   //llave para acceder al token
   TOKEN: 'access_token',
   expiration: 'expiresIn',
